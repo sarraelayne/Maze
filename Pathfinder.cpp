b0c7200919abf402a2e287cda6da8d058193529b
Sarra Smith <sarraelayne@gmail.com>
2018-10-16 01:35:13 +0000

    Basic file structure

---
+++ Pathfinder.cpp
@@ -0,0 +1,6 @@
+#include "Pathfinder.h"
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
---
+++ Pathfinder.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "PathfinderInterface.h"
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Pathfinder: public PathfinderInterface{
+    public:
+	PathfinderInterface() {}
+	~PathfinderInterface() {}
+    string toString() const;
+	void createRandomMaze();
+	bool importMaze(string file_name);
+	vector<string> solveMaze();
+};
